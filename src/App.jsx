78524d5b6cc631aeaf53e64ae36f7942508ee874
Muzammal Ghafoor <muzammalghafoor04@gmail.com>
2025-10-30 21:28:14 -0700

    Navbar completed

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Products from './pages/Products'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Cart from './pages/Cart'
+import Navbar from './components/Navbar'
+import axios from 'axios'
+
+
+const App = () => {
+  const[location,setLocation]=useState()
+  const[openDropDown,setOpenDropDown]=useState(false)
+  const toggleDropdown=()=>{
+    setOpenDropDown(!openDropDown)
+  }
+  const getLocation=async()=>{
+    navigator.geolocation.getCurrentPosition( async pos=>{
+      const{latitude,longitude}=pos.coords
+       const url = `https://nominatim.openstreetmap.org/reverse?lat=${latitude}&lon=${longitude}&format=jsonv2`;
+      try {
+        const location=await axios.get(url)
+        const exactLocation=location.data.address
+        setLocation(exactLocation)
+        setOpenDropDown(false)
+
+        console.log(exactLocation);
+        
+      } catch (error) {
+        
+      }
+      
+    })
+  }
+  useEffect(()=>{
+   getLocation()
+  },[])
+  return (
+    <BrowserRouter>
+    <Navbar location={location} openDropDown={openDropDown} toggleDropdown={toggleDropdown} getLocation={getLocation}/>
+    <Routes>
+     <Route path='/' element={<Home/>}/>
+     <Route path='/products' element={<Products/>}/>
+     <Route path='/about' element={<About/>}/>
+     <Route path='/contact' element={<Contact/>}/>
+     <Route path='/cart' element={<Cart/>}/>
+    </Routes>
+
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,72 @@
+
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react'
+import { MapPin } from 'lucide-react'
+import React from 'react'
+import { CgClose } from 'react-icons/cg'
+import { FaCaretDown } from 'react-icons/fa'
+import { IoCartOutline } from 'react-icons/io5'
+import { Link, NavLink } from 'react-router-dom'
+
+const Navbar = ({location,openDropDown,toggleDropdown,getLocation}) => {
+  
+  
+  return (
+    <div className='bg-white py-3 shadow-2xl'>
+      <div className='max-w-6xl mx-auto flex justify-between items-center'>
+        {/* logo section */}
+        <div className='flex gap-7 items-center'>
+          <Link to={'/'}>
+          <h1 className='font-bold text-3xl'>  <span className='text-red-500 font-serif'>z</span>aptro </h1>
+          </Link>
+          <div className='flex items-center gap-1 cursor-pointer text-gray-700'>
+            < MapPin className='text-red-500' />
+            <span className='font-semibold'>{location?<div className='-space-y-1'>
+            <p>{location.city}</p>
+            <p>{location.state}</p>
+            </div>:'Add Address'}</span>
+            <FaCaretDown onClick={toggleDropdown}/>
+          </div>
+          {
+            openDropDown?
+          <div className='w-[250px] h-max shadow-2xl z-50 bg-white fixed top-16 left-60 border-2 p-5 border-gray-100 rounded-md'>
+           <h1 className='font-semibold mb-4 text-xl flex justify-between '>Change Location
+            <span className='cursor-pointer'><CgClose onClick={toggleDropdown}/></span>
+           </h1>
+           <button onClick={getLocation} className='bg-red-500 text-white px-3 py-1 rounded-md cursor-pointer hover:bg-red-400'>Detect my location</button>
+          </div>:''
+          }
+         
+   
+        </div>
+        {/* menu section */}
+        <nav className='flex gap-7 items-center'>
+          <ul className='flex gap-7 items-center text-xl font-semibold'>
+            <NavLink to={'/'} className={({isActive})=>`${isActive?'border-b-3 transition-all border-red-500':''}`}><li>Home</li></NavLink>
+            <NavLink to={'/products'} className={({isActive})=>`${isActive?'border-b-3 transition-all border-red-500':''}`}><li>Products</li></NavLink>
+            <NavLink to={'/about'} className={({isActive})=>`${isActive?'border-b-3 transition-all border-red-500':''}`}><li>About</li></NavLink>
+            <NavLink to={'/contact'} className={({isActive})=>`${isActive?'border-b-3 transition-all border-red-500':''}`}><li>Contact</li></NavLink>
+            
+          </ul>
+          <Link to={'/cart'} className='relative'>
+          <IoCartOutline  className='w-7 h-7 '/>
+            
+          
+          <span className='absolute px-2 -top-3 left-3 bg-red-500 rounded-full text-white'>0</span>
+          
+          </Link>
+          <div>
+            <SignedOut>
+        <SignInButton className='bg-red-500 text-white px-3 py-1 rounded-md cursor-pointer' />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+          </div>
+            
+        </nav>
+      </div> 
+    </div>
+  )
+}
+
+export default Navbar
